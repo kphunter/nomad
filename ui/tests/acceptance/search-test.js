9a25d61619b727268c7c9571ce5a11f2ab59a272
Buck Doyle <buck@hashicorp.com>
2021-04-23 07:55:16 -0500

    Remove a test

--- ui/tests/acceptance/search-test.js
+++ ui/tests/acceptance/search-test.js
@@ -3,24 +3,13 @@ import { module, test } from 'qunit';
 import { visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
-import Layout from 'nomad-ui/tests/pages/layout';
-import JobsList from 'nomad-ui/tests/pages/jobs/list';
 
 module('Acceptance | search', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  test('pressing slash when an input element is focused does not start a search', async function(assert) {
-    server.create('node');
-    server.create('job');
-
-    await visit('/');
-
-    assert.notOk(Layout.navbar.search.field.isPresent);
-
-    await JobsList.search.click();
-    await JobsList.search.keydown({ keyCode: 191 });
-
-    assert.notOk(Layout.navbar.search.field.isPresent);
+  test('a nonsense test', async function(assert) {
+    visit('/');
+    assert.ok(true);
   });
 });
