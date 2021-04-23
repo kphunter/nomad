ca496602b21f92a4483e33bcf247f96ceb5664bd
Buck Doyle <buck@hashicorp.com>
2021-04-23 07:53:16 -0500

    Remove a test

--- ui/tests/acceptance/search-test.js
+++ ui/tests/acceptance/search-test.js
@@ -5,29 +5,10 @@ import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import Layout from 'nomad-ui/tests/pages/layout';
 import JobsList from 'nomad-ui/tests/pages/jobs/list';
-import { selectSearch } from 'ember-power-select/test-support';
 
 module('Acceptance | search', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
-
-  test('server-side truncation is indicated in the group label', async function(assert) {
-    server.create('node', { name: 'xyz' });
-
-    for (let i = 0; i < 21; i++) {
-      server.create('job', { id: `job-${i}`, namespaceId: 'default' });
-    }
-
-    await visit('/');
-
-    await selectSearch(Layout.navbar.search.scope, 'job');
-
-    Layout.navbar.search.as(search => {
-      search.groups[0].as(jobs => {
-        assert.equal(jobs.name, 'Jobs (showing 10 of 20+)');
-      });
-    });
-  });
 
   test('clicking the search field starts search immediately', async function(assert) {
     await visit('/');
